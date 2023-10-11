4ae70c0b1e58d6bf59da402438c650997c3e4db0
Tareq Newaz Shahriar <tareq.newaz.shahriar@gmail.com>
2023-10-11 21:52:10 +0600

    config updated

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ const secretsText = fs.readFileSync('secrets.json', { encoding: 'utf8' });
 const secretJson = JSON.parse(secretsText);
 
 const config = {};
-config.host = process.env.HOST || secretJson.COSMOS_DB_ENDPOINT; // Get from Azure portal
+config.host = process.env.HOST || secretJson.COSMOS_DB.ENDPOINT; // Get from Azure portal
 config.authKey =
-  process.env.AUTH_KEY || secretJson.COSMOS_DB_AUTH_KEY; // Get from Azure portal
-config.databaseId = "guineapig-db";
+  process.env.AUTH_KEY || secretJson.COSMOS_DB.AUTH_KEY; // Get from Azure portal
+config.databaseId = secretJson.COSMOS_DB.DATABASE_ID; // db name
 config.containerId = "Items";
 
 if (config.host.includes("https://localhost:")) {
